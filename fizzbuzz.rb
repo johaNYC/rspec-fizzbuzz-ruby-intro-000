fbfd65c7da2c6760be91f2e2add8ccb890acf47c
Johana Gutierrez <johagutierrezg@gmail.com>
2020-04-26 23:32:30 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,14 +2,14 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(number)
- elsif (number % 3 == 0) && (number % 5 == 0)
- return "FizzBuzz"if number % 3 == 0
+ if (number % 3 == 0) && (number % 5 == 0)
+ return "FizzBuzz"
+ 
+ elsif number % 3 == 0
  return "Fizz"
  
  elsif number % 5 == 0
  return "Buzz"
- 
- 
  
  else 
    return 
